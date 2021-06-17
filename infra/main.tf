c495f3b8c15ba19187b612c7446bb606434d6bda
Mayank Sharma <mayank.sharma>
2021-06-17 11:26:57 +0530

    Fixed error after removing database

--- infra/main.tf
+++ infra/main.tf
@@ -40,14 +40,14 @@ resource "aws_iam_policy" "github-ci-access-policy" {
 EOF
 }
 
-module "rds" {
+/*module "rds" {
   source = "./rds"
   name_prefix = local.name_prefix
   common_tags = local.common_tags
   subnet_ids = [aws_subnet.subnet-1.id, aws_subnet.subnet-2.id]
   quizzer_sg_id = aws_security_group.quizzer-sg.id
   vpc_id = aws_vpc.vpc.id
-}
+}*/
 
 resource "aws_iam_user_policy_attachment" "github-ci-policy-attachment" {
   user       = aws_iam_user.github-ci.name
